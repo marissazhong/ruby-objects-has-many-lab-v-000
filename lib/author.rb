dd3a102927f31bd27aceb005d7a329a7091d88ab
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 18:56:49 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,6 +5,17 @@ class Author
     @post = []
   end
   def posts
+    @post
+  end
+  def add_post(post)
+    @post << post
+    post.author = self
+  end
+  def add_post_by_title(post_title)
+    new_post = Post.new(post_title)
+    add_post(new_post)
+  end
+  
 
 end
 
