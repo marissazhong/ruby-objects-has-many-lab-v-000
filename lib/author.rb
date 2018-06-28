cc956e765128817b389dec83ab53361d8a5a93ee
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 18:57:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -19,24 +19,3 @@ class Author
     Post.all.length
   end
 end
-
-class Post
-  attr_accessor :title
-  @@all = []
-  def initialize(title)
-    @title = title
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
-  def title
-    @title
-  end
-  def author
-    self.author ? self.author.name : nil
-  end
-  def author_name
-    self.author ? self.author.name : nil
-  end
-end
